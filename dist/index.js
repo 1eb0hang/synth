ea8b31b9e1fcf826ee00d8b1134b75548ab388ea
Lebohang Mokhwibitjane <lebohangmokhwibitjane@gmail.com>
2025-06-08 07:14:46 +0200

    fix: add js back cuz i need it for pages

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+import Audio from "./audio.js";
+import { createAudioStartForm, formEventListener } from "./form.js";
+const audio = new Audio();
+const main = () => {
+    createAudioStartForm();
+    formEventListener(() => { audio.setAudioContext(new AudioContext()); });
+    const keyboard = document.querySelectorAll(".key");
+    if (!keyboard)
+        return;
+    console.log("keboard initialised");
+    for (const key in keyboard) {
+        if (!(keyboard[key] instanceof HTMLInputElement))
+            continue;
+        const self = keyboard[key];
+        self.addEventListener("mousedown", () => {
+            // TODO: check if mousemove while mousedown
+            audio.play(keyboard[key].name);
+        });
+        self.addEventListener("mouseup", () => {
+            //TODO: check if mouse down happened on this key
+            audio.stop(keyboard[key].name);
+        });
+        // TODO: switch to solidjs
+        // TODO: allow mouse drag while mouse down for adjecent key input
+        // TODO: implement wavetype switcing
+        // TODO: implement global filter
+        // TODO: implement user configurable adsr envelope
+        // TODO: implement keyboard input
+        // TODO: implement unison poliphony
+        // TODO: display current key/chord on display
+    }
+};
+main();
