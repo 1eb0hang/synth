ac3fc12329ab0fc0a47a2fb63f48190d29f39f86
Lebohang Mokhwibitjane <lebohangmokhwibitjane@gmail.com>
2025-06-08 07:04:43 +0200

    basic synth

---
+++ README.md
@@ -0,0 +1,5 @@
+# Synth
+
+This is a basic synth created with the web audio api
+Please note that upon entering you must allow the website to 
+initialize the web audio api
---
+++ src/audio.ts
@@ -0,0 +1,119 @@
+import notes from "./notes.js";
+
+interface Envelope{
+    attack:number;
+    decay:number;
+    sustain:number;
+    release:number;
+}
+
+interface Osc{
+    osc:OscillatorNode;
+    gain:GainNode
+}
+
+class Audio{
+    private ctx:AudioContext|undefined;
+    // private gain:GainNode|undefined;
+    private out:AudioDestinationNode|undefined;
+    private readonly envelope:Envelope;
+    private readonly MAX_ENV_TIME:number;
+
+    private readonly PLAYING:{[index:string]:
+        {osc:OscillatorNode, gain:GainNode, playing:boolean}
+    }; 
+
+    constructor(ctx?:AudioContext){
+        this.ctx = ctx;
+        
+        this.envelope = {
+            attack:0.2,
+            decay:1,
+            sustain:0.2,
+            release:0.2
+        };
+
+        this.PLAYING = {};
+        this.MAX_ENV_TIME = 2;
+        if(!ctx) return;
+    }
+
+    readonly setAudioContext = (ctx:AudioContext)=>{
+        this.ctx = ctx;
+        this.out = this.ctx.destination;
+    }
+
+    readonly play = (note:string):void=>{
+        if(!this.ctx || ! this.out) throw new Error("Audio context not set");
+
+        const osc = this.createOsc("triangle", notes[note]);
+        const gain = this.createGain(osc, this.out, 0);
+        
+        gain.gain.setValueAtTime(0, this.ctx.currentTime);
+        
+        this.PLAYING[note] = {osc:osc, gain:gain, playing:true};
+
+        osc.start();
+        this.setGainEnvelopeEnter(gain);
+    }
+
+    readonly stop = (note:string):void =>{
+        this.PLAYING[note].playing = false;
+        const {gain} = this.PLAYING[note];
+        this.setGainEnvelopExit(gain);
+        delete this.PLAYING[note];
+    }
+
+    private readonly createOsc = (type:"sine"|"triangle"|"square"|"sawtooth", frequency:number, detune?:number):OscillatorNode=>{
+        if (!this.ctx){
+            throw new Error("Audio context not set");
+        }
+        const osc = this.ctx?.createOscillator();
+        osc.type = type;
+        osc.frequency.value = frequency;
+        osc.detune.value = detune || 0;
+        return osc;
+    }
+
+    private readonly createGain = (prevNode:AudioNode, nextNode:AudioNode, startingVolume?:number):GainNode=>{
+        if(!this.ctx || !this.out) throw new Error("Audio context not set");
+        
+        const gain = this.ctx.createGain();
+        gain.gain.value = startingVolume||0;
+        prevNode.connect(gain);
+
+        gain.connect(nextNode);
+
+        return gain;
+    }
+
+    private readonly setGainEnvelopeEnter = (gain:GainNode)=>{
+        if(!this.ctx) throw new Error("Audio context not set");
+        
+        const {currentTime} = this.ctx;
+        gain.gain.cancelScheduledValues(currentTime);
+        gain.gain.setValueAtTime(0, currentTime);
+
+        const attackEnd = currentTime+(this.envelope.attack * this.MAX_ENV_TIME);
+        const decayEnd = (this.envelope.decay * this.MAX_ENV_TIME);
+
+        gain.gain.linearRampToValueAtTime(1,attackEnd);
+        gain.gain.setTargetAtTime(this.envelope.sustain, attackEnd, decayEnd);
+    }
+
+    private readonly setGainEnvelopExit = (gain:GainNode)=>{
+        if(!this.ctx) throw new Error("Audio context not set");
+
+        const {currentTime} = this.ctx;
+        const currentGainValue = gain.gain.value;
+
+        gain.gain.cancelScheduledValues(currentTime);
+        gain.gain.setValueAtTime(currentGainValue, currentTime);
+
+        const releaseEnd = currentTime + (this.envelope.release*this.MAX_ENV_TIME);
+
+        gain.gain.linearRampToValueAtTime(0, releaseEnd);
+    }
+}
+
+export default Audio
